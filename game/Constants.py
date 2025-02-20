a1591a0cec780e740a1728ed77a8794984f6131f
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-02-20 10:03:00 +0530

    created a constatnts.py for common games constants

---
+++ game/Constants.py
@@ -0,0 +1,22 @@
+import os
+import random
+
+
+# Colors
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+BLACK = (0, 0, 0)
+
+# Screen dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Player properties
+PLAYER_SIZE = (20, 20)  # Size of the player
+PLAYER_COLOR = RED  # Red color for the player
+
+# Food properties
+FOOD_SIZE = (10, 10)  # Size of the food
+FOOD_COLOR = GREEN  # Green color for the food
