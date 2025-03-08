c9099d2cec98d0121db6a604e6232338f38ea351
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-03-08 10:00:00 +0530

    created a food class

---
+++ game/Food.py
@@ -0,0 +1,28 @@
+import pygame
+import random
+import os
+
+from Constants import *
+
+class Food(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        pass
+
+    def draw(self, screen):
+        pass
+
+    def move(self):
+        pass
+
+    def get_rect(self):
+        pass
+    
+    def get_size(self):
+        pass
+    
+    def get_position(self):
+        pass
+    
+
+    
