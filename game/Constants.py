606ac289d5be3bd95b13a741dc6695378cedf5e5
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-03-19 10:00:00 +0530

    added game to constants

--- game/Constants.py
+++ game/Constants.py
@@ -13,11 +13,13 @@ BLACK = (0, 0, 0)
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+GAME_TICK_RATE = 600  # Frames per second
+
 # Player properties
 PLAYER_SIZE = (20, 20)  # Size of the player
 PLAYER_COLOR = RED  # Red color for the player
-PLAYER_SPEED = 5  # Speed of the player
+PLAYER_SPEED = 1  # Speed of the player
 
 # Food properties
-POINT_SIZE = (10, 10)  # Size of the food
+POINT_SIZE = (20, 20)  # Size of the food
 POINT_COLOR = GREEN  # Green color for the food
