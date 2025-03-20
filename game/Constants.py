47ab9a8e23f156e716ef79a886e1327e350d1c1a
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-03-20 10:00:00 +0530

    updated constants

--- game/Constants.py
+++ game/Constants.py
@@ -8,18 +8,20 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 YELLOW = (255, 255, 0)
 BLACK = (0, 0, 0)
+GRAY = (128, 128, 128)
 
 # Screen dimensions
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 GAME_TICK_RATE = 600  # Frames per second
+BOX_SIZE = 20  # Size of the grid boxes for movement and collision detection
 
 # Player properties
-PLAYER_SIZE = (20, 20)  # Size of the player
+PLAYER_SIZE = (BOX_SIZE, BOX_SIZE)  # Size of the player
 PLAYER_COLOR = RED  # Red color for the player
 PLAYER_SPEED = 1  # Speed of the player
 
 # Food properties
-POINT_SIZE = (20, 20)  # Size of the food
+POINT_SIZE = (BOX_SIZE, BOX_SIZE)  # Size of the food
 POINT_COLOR = GREEN  # Green color for the food
