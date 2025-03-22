ac250b0e42819cca92b5ae269253555ac08a3811
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-03-22 10:00:00 +0530

    added col and row and updated game tick and player speeed

--- game/Constants.py
+++ game/Constants.py
@@ -8,19 +8,22 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 YELLOW = (255, 255, 0)
 BLACK = (0, 0, 0)
-GRAY = (128, 128, 128)
+GRAY = (60, 60, 60)
+
+GAME_TICK_RATE = 15  # Frames per second
+BOX_SIZE = 20  # Size of the grid boxes for movement and collision detection
 
 # Screen dimensions
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-GAME_TICK_RATE = 600  # Frames per second
-BOX_SIZE = 20  # Size of the grid boxes for movement and collision detection
+COLUMNS = SCREEN_WIDTH // BOX_SIZE
+ROWS = SCREEN_HEIGHT // BOX_SIZE
 
 # Player properties
 PLAYER_SIZE = (BOX_SIZE, BOX_SIZE)  # Size of the player
 PLAYER_COLOR = RED  # Red color for the player
-PLAYER_SPEED = 1  # Speed of the player
+PLAYER_SPEED = 20  # Speed of the player
 
 # Food properties
 POINT_SIZE = (BOX_SIZE, BOX_SIZE)  # Size of the food
