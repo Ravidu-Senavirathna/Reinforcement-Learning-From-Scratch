75ec927a12ab7a2e162bc6972d1905d76a9f45dd
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-07-01 20:22:21 +0530

    import stuff for env

--- game/DQN-Agent/env.py
+++ game/DQN-Agent/env.py
@@ -1,7 +1,10 @@
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import Constants
 
-BOX_SIZE    = Constants.BOX_SIZE    # 20px  — one grid cell
-COLUMNS     = Constants.COLUMNS     # 40
-ROWS        = Constants.ROWS        # 30
+import numpy as np
+
+from Player   import Player
+from Point    import Point
+from Obstacle import Obstacle
+
+import config
