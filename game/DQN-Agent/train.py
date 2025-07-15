dc722f19ef6813fe873caa5412d27fe1c726bc6b
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-07-15 20:26:09 +0530

    added loss and state to the loop

--- game/DQN-Agent/train.py
+++ game/DQN-Agent/train.py
@@ -27,13 +27,28 @@ def train():
     scores        = []   # points collected per episode
     losses        = []   # training losses
     best_score    = 0
-    
+
     for episode in range(1, NUM_EPISODES + 1):
         state      = env.reset()
         total_reward = 0.0
         ep_losses    = []
         collected    = 0
 
+
+        done = False
+        while not done:
+            action = agent.act(state)
+            next_state, reward, done, info = env.step(action)
+            agent.remember(state, action, reward, next_state, float(done))
+
+            loss = agent.learn()
+            if loss is not None:
+                ep_losses.append(loss)
+
+            state = next_state
+            total_reward += reward
+            if info.get('collected'):
+                collected += 1
 
 if __name__ == '__main__':
     train()
